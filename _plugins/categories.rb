56cbca41997c0ce490f10cb99b3abcd9c8974894
Jamie Tanna <jamie@jamietanna.co.uk>
2017-03-06 13:08:18 +0000

    Generify the `categories.rb` plugin
    As part of #66.
    
    Remove a number of duplicated sections of code, so we can use a single
    class to model a collection that wants to be generated.

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -1,48 +1,34 @@
 # adapted from Jekyll docs
 module Jekyll
 
-  class CategoryPage < Page
-    def initialize(site, base, dir, category)
+  class CollectionPage < Page
+    def initialize(site, base, dir, collectionKey, collectionList, collectionHtml = 'collection_index.html', collectionPrefixKey = 'collection_title_prefix', collectionPrefix = 'Collection: ')
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
-      self.data['category'] = category
+      self.read_yaml(File.join(base, '_layouts'), collectionHtml)
+      self.data['collectionKey'] = collectionKey
+      self.data['collectionList'] = collectionList
 
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
-      self.data['title'] = "#{category_title_prefix}#{category}"
+      collection_title_prefix = site.config[collectionPrefixKey] || collectionPrefix
+      self.data['title'] = "#{collection_title_prefix}#{collectionKey}"
     end
   end
 
   class CategoryPageGenerator < Generator
     safe true
 
+    # TODO make this generic, too!
     def generate(site)
       if site.layouts.key? 'category_index'
         dir = site.config['category_dir'] || 'categories'
         site.categories.each_key do |category|
-          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+          site.pages << CollectionPage.new(site, site.source, File.join(dir, category), category, site.categories, 'collection_index.html', 'category_title_prefix')
         end
       end
-    end
-  end
-
-  class TagPage < Page
-    def initialize(site, base, dir, tag)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
-      self.data['tag'] = tag
-
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Tag: '
-      self.data['title'] = "#{tag_title_prefix}#{tag}"
     end
   end
 
@@ -53,7 +39,7 @@ module Jekyll
       if site.layouts.key? 'tag_index'
         dir = site.config['tag_dir'] || 'tags'
         site.tags.each_key do |tag|
-          site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
+          site.pages << CollectionPage.new(site, site.source, File.join(dir, tag), tag, site.tags, 'collection_index.html', 'tag_title_prefix')
         end
       end
     end
