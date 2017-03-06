77aab8c47cbe367fa02047662f1dc01d7c3e73ea
Jamie Tanna <jamie@jamietanna.co.uk>
2017-03-06 16:35:33 +0000

    Remove incorrect naming for variables
    As found in !12, we've got a number of variables that are still
    referencing the `collection` naming scheme that we'd chosen before.

--- _plugins/post_metadata_page_generator.rb
+++ _plugins/post_metadata_page_generator.rb
@@ -2,19 +2,19 @@
 module Jekyll
 
   class MetadataPage < Page
-    def initialize(site, base, dir, collectionKey, collectionList, collectionHtml, collectionPrefixKey, collectionPrefix)
+    def initialize(site, base, dir, metadataKey, metadataList, metadataHtml, metadataPrefixKey, metadataPrefix)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), collectionHtml)
-      self.data['collectionKey'] = collectionKey
-      self.data['collectionList'] = collectionList
+      self.read_yaml(File.join(base, '_layouts'), metadataHtml)
+      self.data['metadataKey'] = metadataKey
+      self.data['metadataList'] = metadataList
 
-      collection_title_prefix = site.config[collectionPrefixKey] || collectionPrefix
-      self.data['title'] = "#{collection_title_prefix}#{collectionKey}"
+      metadata_title_prefix = site.config[metadataPrefixKey] || metadataPrefix
+      self.data['title'] = "#{metadata_title_prefix}#{metadataKey}"
     end
   end
 
@@ -22,24 +22,24 @@ module Jekyll
     safe true
 
     def initialize(*args)
-      set_collectionLayout('metadata_page_index')
-      @collectionDirKey = 'collections_dir'
-      @collectionDir = 'collections'
-      @collectionKey = nil
-      @collectionPrefix = 'Collection: '
-      @collectionPrefixKey = 'collection_title_prefix'
+      set_metadataLayout('metadata_page_index')
+      @metadataDirKey = 'metadata_dir'
+      @metadataDir = 'metadata'
+      @metadataKey = nil
+      @metadataPrefix = 'Metadata: '
+      @metadataPrefixKey = 'metadata_title_prefix'
     end
 
-    def set_collectionLayout(key)
-      @collectionLayoutKey = key
-      @collectionLayoutHtml = "#{key}.html"
+    def set_metadataLayout(key)
+      @metadataLayoutKey = key
+      @metadataLayoutHtml = "#{key}.html"
     end
 
     def generate(site)
-      unless @collectionKey.nil?
-        collection = site.post_attr_hash("#{@collectionKey}")
-        collection.each_key do |item|
-          site.pages << MetadataPage.new(site, site.source, File.join(@collectionDir, item), item, collection, @collectionLayoutHtml, @collectionPrefixKey, @collectionPrefix)
+      unless @metadataKey.nil?
+        metadata = site.post_attr_hash("#{@metadataKey}")
+        metadata.each_key do |item|
+          site.pages << MetadataPage.new(site, site.source, File.join(@metadataDir, item), item, metadata, @metadataLayoutHtml, @metadataPrefixKey, @metadataPrefix)
         end
       end
     end
@@ -51,11 +51,11 @@ module Jekyll
       # make sure we use the superclass's attributes
       super(args)
       # and override what we need
-      @collectionDirKey = 'categories_dir'
-      @collectionDir = 'categories'
-      @collectionKey = 'categories'
-      @collectionPrefix = 'Category: '
-      @collectionPrefixKey = 'categories_title_prefix'
+      @metadataDirKey = 'categories_dir'
+      @metadataDir = 'categories'
+      @metadataKey = 'categories'
+      @metadataPrefix = 'Category: '
+      @metadataPrefixKey = 'categories_title_prefix'
     end
   end
 
@@ -65,11 +65,11 @@ module Jekyll
       # make sure we use the superclass's attributes
       super(args)
       # and override what we need
-      @collectionDirKey = 'tags_dir'
-      @collectionDir = 'tags'
-      @collectionKey = 'tags'
-      @collectionPrefix = 'Tag: '
-      @collectionPrefixKey = 'tags_title_prefix'
+      @metadataDirKey = 'tags_dir'
+      @metadataDir = 'tags'
+      @metadataKey = 'tags'
+      @metadataPrefix = 'Tag: '
+      @metadataPrefixKey = 'tags_title_prefix'
     end
   end
 end
