5c78e248cb2141b111fdbaae56c738a749ae164b
Jamie Tanna <jamie@jamietanna.co.uk>
2017-06-14 21:31:20 +0100

    Add proper Cap task for review teardown
    As part of #64.

--- config/deploy/review.rb
+++ config/deploy/review.rb
@@ -1,10 +1,2 @@
 server 'jvt.me', user: 'jvt_me', roles: %w{app}
 set :deploy_to, "/srv/www/jvt.me/jvt.me/review/#{ENV['CI_COMMIT_REF_SLUG']}"
-
-namespace :stop do
-  desc 'Stop the Review App'
-
-  task :cleanup do
-    execute "rm -rf #{fetch:deploy_to}"
-  end
-end
