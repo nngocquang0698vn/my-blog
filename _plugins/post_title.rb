8d309543676a5a8029d132464b99ce6d158210d7
Jamie Tanna <jamie@jamietanna.co.uk>
2018-05-02 21:03:51 +0100

    Import `post_url` from Jekyll
    Via https://github.com/jekyll/jekyll/blob/v3.8.1/lib/jekyll/tags/post_url.rb

---
+++ _plugins/post_title.rb
@@ -0,0 +1,47 @@
+module Jekyll
+  module Tags
+    class PostUrl < Liquid::Tag
+      def initialize(tag_name, post, tokens)
+        super
+        @orig_post = post.strip
+        begin
+          @post = PostComparer.new(@orig_post)
+        rescue StandardError => e
+          raise Jekyll::Errors::PostURLError, <<-MSG
+Could not parse name of post "#{@orig_post}" in tag 'post_url'.
+Make sure the post exists and the name is correct.
+#{e.class}: #{e.message}
+MSG
+        end
+      end
+
+      def render(context)
+        site = context.registers[:site]
+
+        site.posts.docs.each do |p|
+          return p.url if @post == p
+        end
+
+        # New matching method did not match, fall back to old method
+        # with deprecation warning if this matches
+
+        site.posts.docs.each do |p|
+          next unless @post.deprecated_equality p
+          Jekyll::Deprecator.deprecation_message "A call to "\
+            "'{% post_url #{@post.name} %}' did not match " \
+            "a post using the new matching method of checking name " \
+            "(path-date-slug) equality. Please make sure that you " \
+            "change this tag to match the post's name exactly."
+          return p.url
+        end
+
+        raise Jekyll::Errors::PostURLError, <<-MSG
+Could not find post "#{@orig_post}" in tag 'post_url'.
+Make sure the post exists and the name is correct.
+MSG
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag("post_url", Jekyll::Tags::PostUrl)
