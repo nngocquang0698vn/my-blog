22013e7dcddcbce2b0cd25e885071feb2ce299df
Jamie Tanna <jamie@jamietanna.co.uk>
2020-06-09 22:22:46 +0100

    Migrate to new theme

--- .ci/theme-branch.rb
+++ .ci/theme-branch.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-# Ensure that theme is pointing to origin/fork not a branch
-Dir.chdir('themes/tale-hugo') do
+# Ensure that theme is pointing to origin/master not a branch
+Dir.chdir('themes/www.jvt.me-theme') do
   commit = `git rev-parse HEAD`.chomp
-  contains = `git branch fork --contains #{commit}`
-  raise "branch `fork` does not contain commit #{commit}" if contains.length.zero?
+  contains = `git branch master --contains #{commit}`
+  raise "branch `master` does not contain commit #{commit}" if contains.length.zero?
 end
