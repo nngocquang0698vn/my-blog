bdc86d0d613b68034e1593d4c71697e09947adee
Jamie Tanna <jamie@jamietanna.co.uk>
2022-12-09 20:57:44 +0000

    Remove theme checking on branches

--- .ci/theme-branch.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-# Ensure that theme is pointing to origin/master not a branch
-Dir.chdir('themes/www.jvt.me-theme') do
-  commit = `git rev-parse HEAD`.chomp
-  contains = `git branch main --contains #{commit}`
-  raise "branch `main` does not contain commit #{commit}" if contains.length.zero?
-end
